3ccbf5a4c31cf658902269249ad323b96a16169a
anser624 <am24626526jan@gmail.com>
2024-08-21 05:27:31 -0700

    filesadded

---
+++ class.1 js 1.2.3/app.js
@@ -0,0 +1,136 @@
+
+                            // chapter-1-excercise
+
+// alert("hello wolrd")
+// console.log("hello wolrd")
+// var ansar = 125;
+// console.log(ansar)
+// ansar = 5
+// var ansar1 = 5
+
+// console.log(ansar*ansar1)
+
+// var abc2;
+// var abc1;
+// abc1 = 5;
+// abc2 = 6;
+//  var alert1 = "alert external file" ;
+// //  var alert2 ="second alert";
+//  console.log(abc1+abc2)
+//  alert(alert1)
+//  alert(alert2)
+
+                                // chapter-1-excercise
+     // chapter-1-question-1
+       
+    //  <script></script>
+ 
+                                
+     // chapter-1-question-2
+
+     // alert("error please enter a valid password")
+    
+     // chapter-1-question-3
+        
+    // alert("welcome to js land\nhappy coding")
+                            
+    // chapter-1-question-4
+    
+    // alert("welcome to js land")
+    
+    // alert("happy coding\nprevent this creating additional dialogs")
+    
+    // chapter-1-question-5
+    
+    // var console1="hello i can run js through my wb browser console";
+
+    // alert(console1)
+    // console.log(console1)
+                            
+                                // chapter-2-excercise
+
+
+// question.1
+// var username;
+
+// question.2
+
+// var myName = "Anser Malik"
+// alert(myName)
+ 
+// console.log(myName)
+// question.3
+
+// var js = "hello world"
+// alert(js)
+
+// question.4
+
+    // var myname = "Anser Malik"
+    // var age = " age,19"
+    // var course ="web and app development from smit batch-12"
+    //  alert(myname)
+    //  alert(age)
+    //  alert(course)
+
+// question.5
+
+
+// alert("pizza\npizz\npiz\npi\np")
+
+
+// question.6
+
+// var email ="my email adress is\n am24626526malik@gmail.com"
+//  alert(email)    
+
+// question.7
+
+
+// var book = " a  smarter way to learn javascript"
+// var book1 = "i am trying to learn"
+
+// alert(book1 + book)
+
+    // question.8
+
+// document.write ("yeah i can write html content throug javascript")
+
+
+// queestion.9
+
+// var name1 = "꧁𓊈𒆜🆈🅾🆄🆁 🅽🅰🅼🅴𒆜𓊉꧂";
+
+// alert(name1)
+  
+                    //  chapter-3-excercise
+
+//  question-1
+
+// var age ="age = 19"
+// alert(age)
+
+//  question-2
+
+// var visit = 1;
+// alert("you have visited " + visit + " time")
+
+// var visit= 1 + visit ;
+// alert("you have visited " + visit + " time")
+
+// var visit= 1 + visit;
+// alert("you have visited " + visit + " time")
+
+// var visit= 1 + visit ;
+// alert("you have visited " + visit + " time")
+
+// var visit= 1 + visit ;
+// alert("you have visited " + visit + " time")
+
+//  question-3
+
+// var date ="my birth year is 2006"
+
+// document.write(date)
+
+//  question-4
