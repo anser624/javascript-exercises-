baa0bf46d77999701c15acc9a43064dd3b027635
anser624 <am24626526jan@gmail.com>
2024-10-01 04:04:12 -0700

    files added

---
+++ js excercise 12-13/app.js
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////
+//////////===========chapter==12=to=13========/////////
+//////////////////////////////////////////////////////
+
+//////////////////=========question==03===/////////////
+
+// var user1 = +prompt("enter a number to check negative or positiive,")
+//  user1 = user1 % 2 ;
+// if (user1 == 0){
+//     console.log("positive number")
+// }
+// else{
+//     console.log("negative number")
+// }
+
+//////////////////=========question==04===/////////////
+
+// var vowel = prompt("enter any character,","A");
+ 
+// vowel= vowel.toLowerCase();
+
+// if (vowel == "a" || vowel == "e" || vowel == "i" || vowel=="o" || vowel=="u"){
+//     console.log("true!! this is vowel alphabet")
+// }
+// else{
+//     console.log("false!! this is not a vowel alphabet")
+// }
+
+//////////////////=========question==05===/////////////
+
+// var password = "pakistan";
+
+// var password1 = prompt("enter a passoword to see")
+// password1 = password1.toLowerCase();
+
+// if(password == password1){
+
+//     document.write("hurrrry you will win " + "your password correct ,")
+// }
+
+// else{
+//     document.write("its wrong ")
+// }
+
+//////////////////=========question==06===/////////////
+
+// var greeting;
+// var hour = prompt("enter hour ","1 to 24");
+
+// if(hour < 18){
+//     greeting = "good day";
+//     document.write(greeting)
+// }
+// else{
+//     greeting= "good evening";
+//     document.write(greeting)
+// }
+
+//////////////////=========question==07===/////////////
